ad88099de1f0e8ccbee9c0e8d1eaeb4fb475e38f
Nkereuwem <nkereuwem.udoudo1@gmail.com>
2023-09-08 00:25:20 +0100

    second commit

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ from datetime import date, datetime
 import requests
 
 app = Flask(__name__)
-utc_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+@app.route('/')
+def index():
+    return "<h1>Welcome<h1>"
 
 
 @app.route('/api')
-def index():
+def api():
     slack_name = request.args.get("slack_name")
     track = request.args.get("track")
     json_object = {'slack_name': slack_name,
                    'current_day': datetime.now().strftime('%A'),
-                   'utc_time': utc_time,
+                   'utc_time': datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
                    'track': track,
-                   'github_file_url': 'github_file_url',
-                   'github_repo_url': 'github_repo_url',
+                   'github_file_url': 'https://github.com/Nkereuwem03/HNG_Internship/blob/main/main.py',
+                   'github_repo_url': 'https://github.com/Nkereuwem03/HNG_Internship',
                    'status_code': 200}
     return json_object
 
